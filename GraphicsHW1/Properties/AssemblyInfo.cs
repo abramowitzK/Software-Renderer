46cab87c22e6db2fb866de07207e466e88bc539f
Kyle Abramowitz <abramowitzk@gmail.com>
2015-10-17 15:31:46 -0400

    Fixed assembly info.cs

--- GraphicsHW1/Properties/AssemblyInfo.cs
+++ GraphicsHW1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GraphicsHW1")]
+[assembly: AssemblyTitle("GraphicsHW")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GraphicsHW1")]
+[assembly: AssemblyCompany("Kyle Abramowitz")]
+[assembly: AssemblyProduct("GraphicsHW")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
